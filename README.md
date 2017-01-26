adc89676b98fa874d84553779aa72304c50a7d32
Josh Dague <daguej@email.uc.edu>
2017-01-26 15:56:39 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+zcbot
+=====
+
+Automatically posts tomorrow's lunch to a slack channel.
+
+Configuration
+-------------
+Requires these environment variables:
+
+- `ZC_KEY` - log in to your account and get your ID from the URL (eg `https://zerocater.com/m/<ID>`)
+- `SLACK_HOOK` the URL of a Slack [Incoming Webhook](https://slack.com/apps/build/custom-integration).
---
+++ tomorrow.js
@@ -0,0 +1,81 @@
+var request = require('request'),
+	moment = require('moment');
+
+
+var now = moment(),
+	tomorrow = now.clone().add(1, 'days').set({ hour: 0, minute: 0, second: 0, ms: 0 });
+
+if (now.day() == 6 || now.day() == 0) {
+	return console.log('weekend -- bailing');
+}
+
+if (tomorrow.day() == 6) {
+	tomorrow.weekday(8);
+}
+
+var tomorrowEnd = tomorrow.clone().add(1, 'days');
+
+request({
+	url: 'https://api.zerocater.com/v3/companies/' + process.env.ZC_KEY + '/meals',
+	json: true,
+	gzip: true
+}, function(err, res, body) {
+	if (err || res.statusCode != 200) {
+		return console.error(err || res.statusCode + ' ' + body);
+	}
+	for (var i = 0; i < body.length; i++) {
+		if (moment(body[i].time * 1000).isBetween(tomorrow, tomorrowEnd)) {
+			getMenu(body[i].url);
+		}
+	}
+});
+
+function getMenu(url) {
+	request({
+		url: url,
+		json: true,
+		gzip: true
+	}, function(err, res, menu) {
+		if (err || res.statusCode != 200) {
+			return console.error(err || res.statusCode + ' ' + menu);
+		}
+
+		var when = moment(menu.time * 1000);
+
+
+		var attach = [{
+			image_url: menu.vendor_image_url
+		}];
+
+		menu.items.forEach(function(item) {
+			var labels = [];
+			for (var label in item.labels) {
+				if (item.labels[label].value) labels.push(label);
+			}
+			attach.push({
+				title: item.name,
+				text: item.description + (labels.length ? '\n_' + labels.join(', ') + '_' : ''),
+				fallback: item.name + '\n' + item.description + '\n' + labels.join(', '),
+				mrkdwn_in: ["text"]
+			});
+		});
+
+		request({
+			method: 'POST',
+			url: process.env.SLACK_HOOK,
+			json: {
+				username: 'LunchBot',
+				icon_emoji: ':fork_and_knife:',
+				text: '*' + menu.vendor_name + '* - ' + when.format('dddd, MMM Do') + '\n' + menu.vendor_description,
+				attachments: attach
+			}
+		}, function(err, slackRes, body) {
+			if (err || slackRes.statusCode != 200) {
+				console.error(err || slackRes.statusCode + ' ' + body);
+			} else {
+				console.log('ok');
+			}
+		});
+		
+	});
+}
